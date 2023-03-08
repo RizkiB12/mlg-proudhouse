1bf2a3ac9e11be312a2b45ff8e694ef405449ff8
RizkiB12 <rbagus306@gmail.com>
2023-03-08 18:15:54 +0700

    update: add new section for hero

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 //when hamburger menu clicked
 // document.querySelector('#hamburger-menu').onClick = () => {
@@ -7,7 +7,17 @@ const navbarNav = document.querySelector('.navbar-nav');
 //   console.log('clicked');
 // };
 
-document.querySelector('#hamburger-menu').onclick = function() { 
-  navbarNav.classList.toggle('active');
-  console.log('clicked'); 
-}
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+  console.log("clicked");
+};
+
+// cliked out sidebar menu to remove nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
